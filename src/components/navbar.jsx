48b47130ad0e2addc58e0ff676c637e6dbfad097
kazeemkadiri <officialkazeemkadiri@gmail.com>
2025-05-29 10:57:51 +0100

    navbar updates

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar() {
-    return <nav className="flex flex-row justify-between items-center py-5 px-5 w-full">
+    return <nav className="flex flex-row justify-between items-center py-3 w-full">
         <div className="w-auto"><span className="logo rounded-[999px] text-[#007bff] bg-white outline-1 outline-[#007bff] px-[10px] py-[5px]">K</span> Kazeem Kadiri</div>
 
-        <ul className="no-decoration flex flex-row gap-4 no-wrap py-5">
+        <ul className="no-decoration flex flex-row gap-4 no-wrap py-5 text-grey">
             <li className="hover:text-[#007bff] cursor-pointer">About Me</li>
             <li className="hover:text-[#007bff] cursor-pointer">Skills</li>
             <li className="hover:text-[#007bff] cursor-pointer">Hobbies & Interests</li>
